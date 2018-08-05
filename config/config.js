cf2a95b5cc117be733be2052a2c10fe34b334aae
jeaffer villadiego turizo <ing27villadiego@gmail.com>
2018-08-05 13:35:27 -0500

    conexion a la base de datos
    se creo la conexion a la base datos con mongoose y se realizo una conexion exitosa

--- config/config.js
+++ config/config.js
@@ -1,2 +1,3 @@
 export const secret = process.env.SECRET || 'miclavesecreta'
 export const port = process.env.PORT || 3000
+export const mongoUrl = process.env.MONGODB_URI || 'mongodb://localhost/cma-tech'
--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 import Debug from 'debug'
 import http from 'debug'
+import mongoose from 'mongoose'
+
 import app from './app'
 
-import { port } from './config/config'
+import { port, mongoUrl } from './config/config'
 
 const debug = new Debug('cma-tech:root')
 
+mongoose.Promise = global.Promise
 
 async function start() {
+
+  await mongoose.connect(mongoUrl)
 
   app.listen(port, () => {
     debug(`Server running at port ${port}`)
